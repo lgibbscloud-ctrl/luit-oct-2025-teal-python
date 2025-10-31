c3586be1a68e8700457adb0412753fe75bc71927
lgibbscloud-ctrl <lgibbs.cloud@gmail.com>
2025-10-30 22:04:26 -0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,9 @@
-print("Hello World")
+def hello_world():
+    print("Hello World")
+
+if __name__=="__main__":
+    hello_world()
+    hello_world()
+    hello_world()
+    hello_world()
+    hello_world()
